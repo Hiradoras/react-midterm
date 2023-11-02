574279cbbcd010291bb2cfbf83d0268ced5503af
Hiradoras <forwoermind@gmail.com>
2023-11-02 21:42:35 +0600

    made the output text mirrored and made some change

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 Here is site on github-pages:
 https://hiradoras.github.io/react-midterm/
+
+![Alt text](image.png)
--- src/App.js
+++ src/App.js
@@ -3,16 +3,9 @@ import './App.css';
 import { useState } from 'react';
 
 
-
-
-const App = () => {
-  const handleClick = () => {
-    console.log(input)
-  }
+const App = () => {  
   const [input, setInput] = useState('');
-  const [output, setOutput] = useState('');
-
-  const handleMessageChange = event => {
+  const handleInputChange = event => {
     // 👇️ access textarea value
     setInput(event.target.value);
     console.log(event.target.value);
@@ -32,70 +25,26 @@ const App = () => {
           id="input"
           name="inputText"
           value={input}
-          onChange={handleMessageChange}
+          onChange={handleInputChange}
           rows={4}
           cols={40}></textarea>
         </label>
       </div>
-      <div>
-        <label>Output:
+      <div >
+        <label className='App'>Output:
           <textarea
           id="output"
           name="OutputText"
-          value={output}
-          // onChange={handleMessageChange}
+          value={input}
+          onChange={handleInputChange}
           rows={4}
           cols={40}>
           </textarea>
         </label>
       </div>
       <hr />
-
-      <button disabled={!input}
-      onClick={handleClick}
-      >CLICK</button>
     </div>
   );
 };
-
-// function App() {
-//   const [input, setMessage] = useState('');
-
-//   const handleMessageChange = event => {
-//     // 👇️ access textarea value
-//     setMessage(event.target.value);
-//     console.log(event.target.value);
-//   };
-  
-//   return (
-//     <div className="App">
-//       <div>
-//       <header className="App">
-//       <img src={logo} className="App-logo" alt="logo" />
-//       </header>
-//       const [message, setMessage] = useState('initial');
-//       <label>Input:
-//         <textarea 
-//         id = "input"
-//         name="inputText"
-//         value={input}
-//         onChange={handleMessageChange}
-
-//         rows = {4} cols= {40} />
-//       </label>
-//       <label>Output:
-//         <textarea id='transformed-text' name="outputText" rows = {4} cols= {40} />
-//       </label>
-//       </div>
-//       <div>
-//       <button  onClick={() => {
-        
-//         alert('clicked');
-//         }}>
-//           Click</button>
-//           </div>
-//           </div>
-//   );
-// }
 
 export default App;
