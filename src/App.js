62e7a5ca92bd111fa34eac96bb8916f5197eb464
Hiradoras <forwoermind@gmail.com>
2023-11-02 20:34:53 +0600

    Trying to make button react to text area

--- src/App.js
+++ src/App.js
@@ -1,21 +1,100 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+
+
+
+const App = () => {
+  const [input, setInput] = useState('');
+
+  const handleMessageChange = event => {
+    // 👇️ access textarea value
+    setInput(event.target.value);
+    console.log(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App">
-      <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      
-      <label>Input:
-        <textarea name="inputText" rows = {4} cols= {40} />
-      </label>
-      <label>Output:
-        <textarea name="outputText" rows = {4} cols= {40} />
-      </label>
+    <div className='App'>
+      <div>
+        <header
+          className="App">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+      </div>
+      <div>
+        <label>Input:
+          <textarea
+          id="input"
+          name="inputText"
+          value={input}
+          onChange={handleMessageChange}
+          rows={4}
+          cols={40}></textarea>
+        </label>
+      </div>
+      <div>
+        <label>Output:
+          <textarea
+          id="output"
+          name="OutputText"
+          rows={4}
+          cols={40}></textarea>
+        </label>
+      </div>
+      {/* <label htmlFor="message">My Textarea</label>
+      <textarea
+        id="message"
+        name="message"
+        value={input}
+        onChange={handleMessageChange}
+      /> */}
+
+      <hr />
+
+      <button disabled={!input}>CLICK</button>
     </div>
   );
-}
+};
+
+// function App() {
+//   const [input, setMessage] = useState('');
+
+//   const handleMessageChange = event => {
+//     // 👇️ access textarea value
+//     setMessage(event.target.value);
+//     console.log(event.target.value);
+//   };
+  
+//   return (
+//     <div className="App">
+//       <div>
+//       <header className="App">
+//       <img src={logo} className="App-logo" alt="logo" />
+//       </header>
+//       const [message, setMessage] = useState('initial');
+//       <label>Input:
+//         <textarea 
+//         id = "input"
+//         name="inputText"
+//         value={input}
+//         onChange={handleMessageChange}
+
+//         rows = {4} cols= {40} />
+//       </label>
+//       <label>Output:
+//         <textarea id='transformed-text' name="outputText" rows = {4} cols= {40} />
+//       </label>
+//       </div>
+//       <div>
+//       <button  onClick={() => {
+        
+//         alert('clicked');
+//         }}>
+//           Click</button>
+//           </div>
+//           </div>
+//   );
+// }
 
 export default App;
